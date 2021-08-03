999c8f0044f38ec360418265ab82ae09ad5be751
Pratham Arora <arorapratham901@gmail.com>
2021-08-03 23:15:01 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,63 @@
+
+/*
+
+DAY1: Today we learn different types of things
+ie.  1. HTML= Stands for hyper text markup language   (4 or 5 version)
+     2. CSS = Cascading Style Sheets   (2,3) version
+     3. JS = Java script (also know as vanilla js) version Es5(all browser support that) or Es6(Some brownser support that ) 
+
+
+     Language that we use as a backend web dev??
+1. PHP (developed in 2000) 
+    Core /pure PHP
+    Cake PHP
+    Laravel PHP
+    Symphony 
+
+2. Ruby 
+   Ruby on rails 
+   
+3. Python
+   Django   
+
+4. Js 
+   Nodejs
+
+     //Question based upon that -
+     Q-1 Why we used html,css and js in web dev??
+     Ans Beacuse html and css are backbone of web dev (through that we can design basic webpage)
+     if we want to make a webpage dynamic ,then we use js. Without that webpage look like very ugly.
+
+Q-2 In this why we use js i.e nodejs in backend??
+Ans Nodejs is generally a javascript run time environment that we used in backend (at server)
+    Nodejs is fast 
+    Its easy for programmer to learn only one language (server side as well as client side )
+    That's why we are using nodejs as backend language in this training
+
+
+    There different of library of js through that our life become very easy
+    1. React
+    2. Vue
+    3. Angular
+
+    
+
+    Q-1 How website work??
+
+    Ans let suppose we search a website google.com after that request send to server (that present anywhere in world)
+    The server will serve us html, css (client side )
+    Js =Let suppose we click a button it will show a alert (this we will do it with js )
+
+    backend =Suppose when you enter detail in a form ,and submit it. It will store in database  (we do it with nodejs ) 
+*/
+
+
+
+/*
+
+"My name is pratham"    //Its a string i.e Collection of characters
+192                     //That's no in cs we called as int datatype
+*/
+console.log("HELLO WORLD");     
+console.log("HELLO _h");
+console.log("Hello world"+19);
