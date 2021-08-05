4f3846f58ab453d13db1c70ea5691e3564984ce6
Pratham Arora <arorapratham901@gmail.com>
2021-08-05 11:36:44 +0530

    3rd commot

---
+++ division.js
@@ -0,0 +1,5 @@
+let a=124;
+let b=3;
+
+let result=a%b;
+console.log("the result of two no is",result);
---
+++ multiply.js
@@ -0,0 +1,9 @@
+console.log("Hello my name is pratham ");
+
+//Muliple a no 
+
+let a=100;
+let b=100;
+
+let result=a*b;
+console.log(result);
